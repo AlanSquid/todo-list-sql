99836147a228e600a7d338d6a111682c53ff2aed
AlanSquid <squid78313@gmail.com>
2022-11-15 16:29:04 +0800

    add express.Router. add connect-flash and partails to display different erorrs message

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+const todos = require('./modules/todos')
+const users = require('./modules/users')
+// const auth = require('./modules/auth.js')
+
+const { authenticator } = require('../middleware/auth')
+
+router.use('/users', users)
+router.use('/todos', authenticator, todos)
+// router.use('/auth', auth)
+router.use('/', authenticator, home)
+
+module.exports = router
---
+++ routes/modules/home.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const Todo = require('../../models/todo')
+
+router.get('/', (req, res) => {
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+module.exports = router
